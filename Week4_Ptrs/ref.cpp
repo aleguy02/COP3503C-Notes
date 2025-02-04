cfc9ebc4d06724d467c6fb2ee0f8611d3cd25adb
Ale V <alejandro.the.villate@gmail.com>
2025-02-04 14:20:20 -0500

    Add looping demo to ref.cpp

--- Week4_Ptrs/ref.cpp
+++ Week4_Ptrs/ref.cpp
@@ -5,10 +5,31 @@
 
 int main()
 {
-    int temp = 5;
-    int& ref = temp;
-    std::cout << &temp << "\n" << &ref << std::endl;
+    // Modifying references
+    {
+        int temp = 5;
+        int& ref = temp;
+        std::cout << &temp << "\n" << &ref << std::endl;
 
-    int temp2 = 100;
-    ref = temp2;
+        int temp2 = 100;
+        ref = temp2;
+    }
+    std::cout << std::endl;
+
+    // looping with auto, also demonstrating references
+    {
+        int arr_size = 5;
+        int arr[arr_size] {1, 2, 3, 4, 5};
+
+        // modify each reference
+        for (auto& el: arr) {  // Try removing the ampersand to see what happens
+            el += 10;
+            std::cout << el << std::endl;
+        }
+
+        std::cout << "\n";
+        for (auto el: arr) {
+            std::cout << el << std::endl;
+        }
+    }
 }
